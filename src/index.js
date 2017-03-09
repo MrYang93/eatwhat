fc9ad9ac960eacae78b3c29e558ca5c7712c5bc4
ybl <786429442@qq.com>
2017-03-09 16:18:52 +0800

    add

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import EatWhat from './EatWhat'
-import './main.css';
-
-ReactDOM.render(
-  <EatWhat />,document.getElementById('root'));
